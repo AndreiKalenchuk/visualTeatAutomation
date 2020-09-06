35c810672ce576b526a9f1d902506b71a23ac2b3
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-09-05 21:44:20 -0400

    created wdio-souce.conf.js file to separate config files for running tests on local machine.

---
+++ test/specs/testsV1/task-1.js
@@ -0,0 +1,5 @@
+describe('Cress-device elements', () => {
+    it('should show Search field on laptops and tablets but hide it an mobile', () => {
+        
+    });
+})
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+let timeout = (process.env.DEBUG === 'true') ? 99999999 : 120000;
+
 exports.config = {
     //
     // ====================
@@ -7,22 +9,7 @@ exports.config = {
     // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
     // on a remote machine).
     runner: 'local',
-    //
-    // =================
-    // Service Providers
-    // =================
-    // WebdriverIO supports Sauce Labs, Browserstack, Testing Bot and LambdaTest (other cloud providers
-    // should work too though). These services define specific user and key (or access key)
-    // values you need to put in here in order to connect to these services.
-    //
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
-    //
-    // If you run your tests on Sauce Labs you can specify the region you want to run your tests
-    // in via the `region` property. Available short handles for regions are `us` (default) and `eu`.
-    // These regions are used for the Sauce Labs VM cloud and the Sauce Labs Real Device Cloud.
-    // If you don't provide the region it will default for the `us`
-    region: 'us',
+
     //
     // ==================
     // Specify Test Files
@@ -122,7 +109,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver','sauce','applitools'],
+    services: ['chromedriver','applitools'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -150,7 +137,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout,
     },
     //
     // =====
