54d1c4582b499d5ea13fcef0c9a94d5e8637a8e5
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-09-05 23:16:20 -0400

    created component header, add Page object for Home page test verify input field home page

--- test/specs/testsV1/task-1.js
+++ test/specs/testsV1/task-1.js
@@ -1,5 +1,26 @@
+const {viewports} = require("../../../utils/constans")
+const HomePage = require('../../pageObjects/HomePage')
+
 describe('Cress-device elements', () => {
-    it('should show Search field on laptops and tablets but hide it an mobile', () => {
-        
+    before('', () => {
+        browser.url('./gridHackathonV1.html')
+    })
+
+    describe('LAPTOP VIEW', () => {
+        before('', () => {
+            browser.setWindowSize(viewports.laptop.width, viewports.laptop.height)
+        });
+
+        it('should show Search field on laptops and tablets but hide it an mobile', () => {
+            expect(HomePage.header.searchBtnSubmit).toBeDisabled();
+            browser.pause(4000)
+        });
     });
+
+    describe('TABLET VIEW', () => {
+    });
+
+    describe('MOBILE VIEW', () => {
+    });
+
 })
---
+++ utils/constans.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    viewports: {
+        laptop: {
+            width: 1200,
+            height: 700
+        },
+        tablet: {
+            width: 768,
+            height: 700
+        },
+        mobile: {
+            width: 500,
+            height: 700
+        },
+    }
+
+};
